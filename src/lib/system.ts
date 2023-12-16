e3c67d15505038208c1ca92bd08ea9d9842490ce
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-16 22:46:34 +0200

    *

--- src/lib/system.ts
+++ src/lib/system.ts
@@ -1,26 +1,34 @@
 import { ErrorEnvNotSupported } from "shared/error-handler";
 import { EnvironmentType } from "shared/is-env";
 
+import fast_glob from "fast-glob";
+
+type ListenerEventType = "rename" | "change" | "error" | "close";
+type ListenerEventFileNameType = Error | string | null;
+
+type Listener = (
+  event: ListenerEventType,
+  filename: ListenerEventFileNameType,
+) => void;
+
 class FileSystem {
   private env: EnvironmentType = EnvironmentType.node;
   constructor(env: EnvironmentType) {
     this.env = env;
   }
 
-  public async readFile(path: string): Promise<string> {
-    if (this.env === EnvironmentType.node) {
-      const fs = await import("fs");
-      return fs.readFileSync(path, "utf-8");
-    }
-
-    if (this.env === EnvironmentType.bun) {
-      return Bun.file(path).text();
-    }
-
+  public async listen(dirname: string, listener: Listener): Promise<void> {
     switch (this.env) {
-      case EnvironmentType.browser:
-      case EnvironmentType.deno: {
-        throw ErrorEnvNotSupported(this.env);
+      case EnvironmentType.node:
+      case EnvironmentType.bun: {
+        const fs = await import("fs");
+        const watcher = fs.watch(dirname, listener);
+        process.on("SIGINT", () => {
+          // close watcher when Ctrl-C is pressed
+          watcher.close();
+          process.exit(0);
+        });
+        break;
       }
       default: {
         throw ErrorEnvNotSupported(this.env);
@@ -28,17 +36,27 @@ class FileSystem {
     }
   }
 
-  public async cwd(): Promise<string> {
-    if (this.env === EnvironmentType.node) {
-      const process = await import("process");
-      return process.cwd();
-    }
-
-    if (this.env === EnvironmentType.bun) {
-      return import.meta.dir;
-    }
-
+  public async readFile(path: string): Promise<string> {
     switch (this.env) {
+      case EnvironmentType.node: {
+        const fs = await import("fs");
+        return fs.readFileSync(path, "utf-8");
+      }
+      case EnvironmentType.bun: {
+        return Bun.file(path).text();
+      }
+      default:
+        throw ErrorEnvNotSupported(this.env);
+    }
+  }
+
+  public async cwd(): Promise<string> {
+    switch (this.env) {
+      case EnvironmentType.node:
+      case EnvironmentType.bun: {
+        const process = await import("process");
+        return process.cwd();
+      }
       case EnvironmentType.browser:
       case EnvironmentType.deno: {
         throw ErrorEnvNotSupported(this.env);
@@ -50,6 +68,35 @@ class FileSystem {
   }
 }
 
-class System {}
+class System {
+  private env: EnvironmentType = EnvironmentType.node;
+  constructor(env: EnvironmentType) {
+    this.env = env;
+  }
+
+  async loadFiles(pathname: string, extension: string): Promise<string[]> {
+    const completePath = `${pathname}/**/*${extension}`;
+    switch (this.env) {
+      case EnvironmentType.node: {
+        const files = await fast_glob.async(completePath, {
+          ignore: ["**/node_modules/**"],
+        });
+        return files;
+      }
+      case EnvironmentType.bun: {
+        const glob = new Bun.Glob(extension);
+        const files = await Array.fromAsync(
+          glob.scan({
+            cwd: pathname,
+            absolute: true,
+          }),
+        );
+        return files;
+      }
+      default:
+        throw ErrorEnvNotSupported(this.env);
+    }
+  }
+}
 
 export { FileSystem, System };
