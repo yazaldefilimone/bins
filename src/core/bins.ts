e33c619f6e67aa773f5eaba25912e8c1b4b81948
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-12-22 19:30:35 +0200

    *

--- src/core/bins.ts
+++ src/core/bins.ts
@@ -2,7 +2,7 @@ import { ErrorEnvNotSupported } from "shared/error-handler";
 import { isEnv } from "shared/is-env";
 import { isSuporte } from "shared/suporte";
 import { FileSystem, System } from "lib/system";
-
+import { run } from "core/run";
 const env = isEnv();
 const suporte = isSuporte(env);
 console.log(env, suporte);
@@ -12,21 +12,36 @@ if (!suporte) {
 if (suporte.isSupported === false) {
   throw ErrorEnvNotSupported(suporte.name);
 }
+const fs = new FileSystem(env);
+const system = new System(env);
 
-async function boostrap() {
-  const fs = new FileSystem(env);
-  const system = new System(env);
+async function load() {
   const absolutePath = await fs.cwd();
-  const file = await fs.readFile("package.json");
-  console.log(absolutePath);
-  console.log(file);
-
-  // await fs.listen(".", (event, filename) => {
-  //   console.log(`event is: ${event} filename provided: ${filename}`);
-  // });
-
-  const files = await system.loadFiles(`${absolutePath}/src`, "**/*.ts");
-  console.log(files);
+  const testsfiles = await system.loadFiles(
+    `${absolutePath}/examples`,
+    ".test.ts",
+  );
+  return {
+    testsfiles,
+    testFolder: `${absolutePath}/examples`,
+  };
 }
 
-boostrap();
+fs.listen(".", (event, filename) => {
+  console.log(`event is: ${event} filename provided: ${filename}`);
+});
+
+async function bootstrap() {
+  const { testsfiles, testFolder } = await load();
+  console.log(testsfiles);
+  for (const file of testsfiles) {
+    const { isSuccessful, error } = await run(file, fs.readFile.bind(fs), env);
+    if (isSuccessful) {
+      console.log(`Test file ${file} passed`);
+    } else {
+      console.log(`Test file ${file} failed`);
+    }
+  }
+}
+
+bootstrap();
